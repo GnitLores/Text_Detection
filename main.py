f8a9f7ec7d51ea2243d16230f2795d9de56e50e2
GnitLores <11030459+GnitLores@users.noreply.github.com>
2022-12-11 12:26:06 +0100

    Remove small and large connected components

--- main.py
+++ main.py
@@ -72,49 +72,83 @@ for image in images:
 
 
     fig, ax = plt.subplot_mosaic([
-        ["Threshold", 'Horizontal', "Vertical"]
+        ["Threshold", 'Components', "Removed"]
         ], figsize=(1600/myDpi, 1000/myDpi), dpi=myDpi)
 
     thresh = cv2.threshold(blackhat, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
     makeSubplot(thresh, ax, "Threshold", title="Threshold")
 
-    # Remove horizontal
-    horizontal_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (25,1))
-    detected_lines = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, horizontal_kernel, iterations=2)
-    cnts = cv2.findContours(detected_lines, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    cnts = cnts[0] if len(cnts) == 2 else cnts[1]
-    for c in cnts:
-        cv2.drawContours(blackhat, [c], -1, (0,0,0), 2)
-    makeSubplot(blackhat, ax, "Horizontal", title="Horizontal")
+    # Apply the Component analysis function
+    analysis = cv2.connectedComponentsWithStats(thresh, 4, cv2.CV_32S)
+    (totalLabels, label_ids, values, centroid) = analysis
+    # Initialize a new image to
+    # store all the output components
+    outputMask = np.zeros(thresh.shape, dtype="uint8")
+    # Loop through each component
+    for i in range(1, totalLabels):
+        area = values[i, cv2.CC_STAT_AREA] 
+    
+        if area < 10 or area > 250:
+            # Labels stores all the IDs of the components on the each pixel
+            # It has the same dimension as the threshold
+            # So we'll check the component
+            # then convert it to 255 value to mark it white
+            componentMask = (label_ids == i).astype("uint8") * 255
+            
+            # Creating the Final output mask
+            outputMask = cv2.bitwise_or(outputMask, componentMask)
+    makeSubplot(outputMask, ax, "Components", title="Components")
 
-    # Remove vertical
-    vertical_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1, 25))
-    detected_lines = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, vertical_kernel, iterations=2)
-    cnts = cv2.findContours(detected_lines, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    cnts = cnts[0] if len(cnts) == 2 else cnts[1]
-    for c in cnts:
-        cv2.drawContours(blackhat, [c], -1, (0,0,0), 2)
-    makeSubplot(blackhat, ax, "Vertical", title="Vertical")
+    masked = cv2.subtract(thresh, outputMask)
+    makeSubplot(masked, ax, "Removed", title="Removed")
 
-    # # compute the Scharr gradient of the blackhat image  in the y direction and scale the
-	# # result into the range [0, 255]
-    # gradY = cv2.Sobel(blackhat, ddepth=cv2.CV_32F, dx=0, dy=1, ksize=-1)
-    # gradY = np.absolute(gradY)
-    # (minVal, maxVal) = (np.min(gradY), np.max(gradY))
-    # gradY = (255 * ((gradY - minVal) / (maxVal - minVal))).astype("uint8")
-    # makeSubplot(gradY, ax, "Gradient", title="Gradient")
+
+
 
     # fig, ax = plt.subplot_mosaic([
-    #     ['Close', "Threshold", "Gradient"]
+    #     ["Threshold", 'Horizontal', "Vertical"]
     #     ], figsize=(1600/myDpi, 1000/myDpi), dpi=myDpi)
 
-    # # apply a closing operation using the rectangular kernel to close
-	# # gaps in between letters
-    # gradX = cv2.morphologyEx(gradX, cv2.MORPH_CLOSE, rectKernel)
-    # makeSubplot(gradX, ax, "Close", title="Close")
-
-    # # apply Otsu's thresholding method
-    # thresh = cv2.threshold(gradX, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+    # thresh = cv2.threshold(blackhat, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
     # makeSubplot(thresh, ax, "Threshold", title="Threshold")
+
+    # # Remove horizontal
+    # horizontal_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (25,1))
+    # detected_lines = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, horizontal_kernel, iterations=2)
+    # cnts = cv2.findContours(detected_lines, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    # cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+    # for c in cnts:
+    #     cv2.drawContours(blackhat, [c], -1, (0,0,0), 2)
+    # makeSubplot(blackhat, ax, "Horizontal", title="Horizontal")
+
+    # # Remove vertical
+    # vertical_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1, 25))
+    # detected_lines = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, vertical_kernel, iterations=2)
+    # cnts = cv2.findContours(detected_lines, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    # cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+    # for c in cnts:
+    #     cv2.drawContours(blackhat, [c], -1, (0,0,0), 2)
+    # makeSubplot(blackhat, ax, "Vertical", title="Vertical")
+
+    # # # compute the Scharr gradient of the blackhat image  in the y direction and scale the
+	# # # result into the range [0, 255]
+    # # gradY = cv2.Sobel(blackhat, ddepth=cv2.CV_32F, dx=0, dy=1, ksize=-1)
+    # # gradY = np.absolute(gradY)
+    # # (minVal, maxVal) = (np.min(gradY), np.max(gradY))
+    # # gradY = (255 * ((gradY - minVal) / (maxVal - minVal))).astype("uint8")
+    # # makeSubplot(gradY, ax, "Gradient", title="Gradient")
+
+    # # fig, ax = plt.subplot_mosaic([
+    # #     ['Close', "Threshold", "Gradient"]
+    # #     ], figsize=(1600/myDpi, 1000/myDpi), dpi=myDpi)
+
+    # # # apply a closing operation using the rectangular kernel to close
+	# # # gaps in between letters
+    # # gradX = cv2.morphologyEx(gradX, cv2.MORPH_CLOSE, rectKernel)
+    # # makeSubplot(gradX, ax, "Close", title="Close")
+
+    # # # apply Otsu's thresholding method
+    # # thresh = cv2.threshold(gradX, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+    # # makeSubplot(thresh, ax, "Threshold", title="Threshold")
 
 plt.show()
