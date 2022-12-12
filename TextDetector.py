fab5232e074c33f2191bc62078637c810150145a
GnitLores <11030459+GnitLores@users.noreply.github.com>
2022-12-12 09:35:36 +0100

    Extract secondary processing to class method and add horizontal line removal

--- TextDetector.py
+++ TextDetector.py
@@ -15,17 +15,19 @@ class TextDetector:
     def detect_text(self):
         self.__preprocessing()
         self.__remove_nonchars()
+        self.__secondary_processing()
 
         if self.do_visualize: plt.show()
 
-    def __make_subplot_figure(self, subplot_keys):
+    def __make_subplot_figure(self, subplot_keys, title = ""):
         dpi = 96
         figure_width = 1600
         figure_height = 1000
         fig, ax = plt.subplot_mosaic([subplot_keys], figsize = (figure_width / dpi, figure_height / dpi), dpi = dpi)
+        fig.suptitle(title)
         return fig, ax
 
-    def __makeSubplot(self, image, ax, key, colormap = "gray", title = ""):
+    def __make_subplot(self, image, ax, key, colormap = "gray", title = ""):
         ax[key].imshow(image, cmap=colormap)
         ax[key].set_title(title)
         ax[key].axis('off')
@@ -34,8 +36,8 @@ class TextDetector:
         
         if self.do_visualize:
             key1, key2, key3 = "Threshold", 'Components', "Removed"
-            fig, ax = self.__make_subplot_figure([key1, key2, key3])
-        if self.do_visualize: self.__makeSubplot(self.original_image, ax, key1, title = "Original Image")
+            fig, ax = self.__make_subplot_figure([key1, key2, key3], title = "1: Preprocessing")
+        if self.do_visualize: self.__make_subplot(self.original_image, ax, key1, title = "Original Image")
         
         # Resize to uniform width
         self.image = imutils.resize(self.original_image, width = self.processed_width)
@@ -45,23 +47,23 @@ class TextDetector:
 
         # smooth the image using Gaussian to reduce high frequeny noise
         self.image = cv2.GaussianBlur(self.image, (3, 3), 0)
-        if self.do_visualize: self.__makeSubplot(self.image, ax, key2, title = "Rescaled, Greyscale, Blurred image")
+        if self.do_visualize: self.__make_subplot(self.image, ax, key2, title = "Rescaled, Greyscale, Blurred image")
 
         # Blackhat - enhances dark objects of interest in a bright background.
         # The black-hat transform is defined as the difference between the closing and the input image.
         rectKernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 13))
         self.image = cv2.morphologyEx(self.image, cv2.MORPH_BLACKHAT, rectKernel)
-        if self.do_visualize: self.__makeSubplot(self.image, ax, key3, title = "Blackhat Transform")
+        if self.do_visualize: self.__make_subplot(self.image, ax, key3, title = "Blackhat Transform")
 
     def __remove_nonchars(self):
         
         if self.do_visualize:
             key1, key2, key3 = "Threshold", 'Components', "Removed"
-            fig, ax = self.__make_subplot_figure([key1, key2, key3])
+            fig, ax = self.__make_subplot_figure([key1, key2, key3], title = "2: Remove non-chars")
 
         # Get black/white image with otsu threshold
         self.image = cv2.threshold(self.image, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
-        if self.do_visualize: self.__makeSubplot(self.image, ax, key1, title = "Thresholded image")
+        if self.do_visualize: self.__make_subplot(self.image, ax, key1, title = "Thresholded image")
 
         # Create mask of components that are very unlike characters
         analysis = cv2.connectedComponentsWithStats(self.image, 4, cv2.CV_32S)
@@ -77,8 +79,31 @@ class TextDetector:
             if area < 10 or area > 250 or width > 3 * height:
                 componentMask = (label_ids == i).astype("uint8") * 255 # Convert component pixels to 255 to mark white
                 outputMask = cv2.bitwise_or(outputMask, componentMask) # Add component to mask
-        if self.do_visualize: self.__makeSubplot(outputMask, ax, key2, title = "Rejected Components")
+        if self.do_visualize: self.__make_subplot(outputMask, ax, key2, title = "Rejected Components")
 
         # Subtract mask from image
         self.image = cv2.subtract(self.image, outputMask)
-        if self.do_visualize: self.__makeSubplot(self.image, ax, key3, title = "Non-chars filtered")
+        if self.do_visualize: self.__make_subplot(self.image, ax, key3, title = "Non-chars filtered")
+
+    def __secondary_processing(self):
+        if self.do_visualize:
+            key1, key2, key3 = "RemoveVerticalLines", 'RemoveHorizontalLines', "Removed"
+            fig, ax = self.__make_subplot_figure([key1, key2, key3], title = "3: Secondary processing")
+
+        def remove_line(remove_vertical = True):
+            kernel_size = (1, 25)
+            if not remove_vertical: kernel_size = kernel_size[::-1]
+            vertical_line_removal_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, kernel_size)
+            detected_lines = cv2.morphologyEx(self.image, cv2.MORPH_OPEN, vertical_line_removal_kernel, iterations = 2)
+            cnts = cv2.findContours(detected_lines, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+            for c in cnts:
+                cv2.drawContours(self.image, [c], -1, (0, 0, 0), 2)
+
+        # Remove vertical lines
+        remove_line(True)
+        if self.do_visualize: self.__make_subplot(self.image, ax, key1, title = "Remove Vertical Lines")
+
+        # Remove horizontal lines
+        remove_line(False)
+        if self.do_visualize: self.__make_subplot(self.image, ax, key2, title = "Remove Horizontal Lines")
