87c746ce59436ec620880afe8c34e4c3c00cb960
GnitLores <11030459+GnitLores@users.noreply.github.com>
2022-12-12 09:48:25 +0100

    extract sentence filtering to class method

--- TextDetector.py
+++ TextDetector.py
@@ -13,9 +13,10 @@ class TextDetector:
         self.processed_width = 600
         
     def detect_text(self):
-        self.__preprocessing()
-        self.__remove_nonchars()
-        self.__secondary_processing()
+        self.__preprocess()
+        self.__filter_chars()
+        self.__process_secondary()
+        self.__filter_sentences()
 
         if self.do_visualize: plt.show()
 
@@ -32,10 +33,10 @@ class TextDetector:
         ax[key].set_title(title)
         ax[key].axis('off')
 
-    def __preprocessing(self):
+    def __preprocess(self):
         
         if self.do_visualize:
-            key1, key2, key3 = "Threshold", 'Components', "Removed"
+            key1, key2, key3 = "1", '2', "3"
             fig, ax = self.__make_subplot_figure([key1, key2, key3], title = "1: Preprocessing")
         if self.do_visualize: self.__make_subplot(self.original_image, ax, key1, title = "Original Image")
         
@@ -55,11 +56,11 @@ class TextDetector:
         self.image = cv2.morphologyEx(self.image, cv2.MORPH_BLACKHAT, rectKernel)
         if self.do_visualize: self.__make_subplot(self.image, ax, key3, title = "Blackhat Transform")
 
-    def __remove_nonchars(self):
+    def __filter_chars(self):
         
         if self.do_visualize:
-            key1, key2, key3 = "Threshold", 'Components', "Removed"
-            fig, ax = self.__make_subplot_figure([key1, key2, key3], title = "2: Remove non-chars")
+            key1, key2, key3 = "1", '2', "3"
+            fig, ax = self.__make_subplot_figure([key1, key2, key3], title = "2: Filter Characters")
 
         # Get black/white image with otsu threshold
         self.image = cv2.threshold(self.image, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
@@ -68,9 +69,7 @@ class TextDetector:
         # Create mask of components that are very unlike characters
         analysis = cv2.connectedComponentsWithStats(self.image, 4, cv2.CV_32S)
         (totalLabels, label_ids, values, centroid) = analysis
-
-        outputMask = np.zeros(self.image.shape, dtype="uint8") # Mask to remove
-
+        outputMask = np.zeros(self.image.shape, dtype = "uint8") # Mask to remove
         for i in range(1, totalLabels): # Check each component
             area = values[i, cv2.CC_STAT_AREA] 
             width = values[i, cv2.CC_STAT_WIDTH]
@@ -79,15 +78,15 @@ class TextDetector:
             if area < 10 or area > 250 or width > 3 * height:
                 componentMask = (label_ids == i).astype("uint8") * 255 # Convert component pixels to 255 to mark white
                 outputMask = cv2.bitwise_or(outputMask, componentMask) # Add component to mask
-        if self.do_visualize: self.__make_subplot(outputMask, ax, key2, title = "Rejected Components")
+        if self.do_visualize: self.__make_subplot(outputMask, ax, key2, title = "Non-character Components")
 
         # Subtract mask from image
         self.image = cv2.subtract(self.image, outputMask)
-        if self.do_visualize: self.__make_subplot(self.image, ax, key3, title = "Non-chars filtered")
+        if self.do_visualize: self.__make_subplot(self.image, ax, key3, title = "Characters filtered")
 
-    def __secondary_processing(self):
+    def __process_secondary(self):
         if self.do_visualize:
-            key1, key2, key3 = "RemoveVerticalLines", 'RemoveHorizontalLines', "Removed"
+            key1, key2, key3 = "1", '2', "3"
             fig, ax = self.__make_subplot_figure([key1, key2, key3], title = "3: Secondary processing")
 
         def remove_line(remove_vertical = True):
@@ -107,3 +106,31 @@ class TextDetector:
         # Remove horizontal lines
         remove_line(False)
         if self.do_visualize: self.__make_subplot(self.image, ax, key2, title = "Remove Horizontal Lines")
+
+    def __filter_sentences(self):
+        if self.do_visualize:
+            key1, key2, key3 = "1", '2', "3"
+            fig, ax = self.__make_subplot_figure([key1, key2, key3], title = "4: Filter sentences")
+
+        # apply a closing operation using a rectangular kernel to close
+        # gaps in between letters
+        sentenceKernel = cv2.getStructuringElement(cv2.MORPH_RECT, (2, 13))
+        self.image = cv2.morphologyEx(self.image, cv2.MORPH_CLOSE, sentenceKernel)
+        if self.do_visualize: self.__make_subplot(self.image, ax, key1, title = "Join Sentences")
+
+        # Create mask of components that are very unlike sentences
+        analysis = cv2.connectedComponentsWithStats(self.image, 4, cv2.CV_32S)
+        (totalLabels, label_ids, values, centroid) = analysis
+        outputMask = np.zeros(self.image.shape, dtype="uint8") # Mask to remove
+        for i in range(1, totalLabels):  # Check each component
+            area = values[i, cv2.CC_STAT_AREA]
+            width = values[i, cv2.CC_STAT_WIDTH]
+            height = values[i, cv2.CC_STAT_HEIGHT]
+        
+            if width < 10 or height < 10 or width > height or area < width * height * 0.3:
+                componentMask = (label_ids == i).astype("uint8") * 255 # Convert component pixels to 255 to mark white
+                outputMask = cv2.bitwise_or(outputMask, componentMask) # Add component to mask
+        if self.do_visualize: self.__make_subplot(outputMask, ax, key2, title = "Non-sentence Components")
+
+        self.image = cv2.subtract(self.image, outputMask)
+        if self.do_visualize: self.__make_subplot(self.image, ax, key3, title = "Sentences Filtered")
